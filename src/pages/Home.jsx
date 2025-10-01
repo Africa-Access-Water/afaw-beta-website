2a5cd506144d81750a7ea2715893a41d1ccb96d7
6upe <katongobupe444@gmail.com>
2025-09-30 21:14:31 -0700

    Task: Projects Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
 
     const PRODUCTION_API_BASE = "https://afaw-beta-api.onrender.com/api/";
     const LOCAL_API_BASE = "http://localhost:5000/api";
-    const API_BASE = LOCAL_API_BASE; // switch to LOCAL_API_BASE for local dev
+    const API_BASE = PRODUCTION_API_BASE; // switch to LOCAL_API_BASE for local dev
 
     const [projects, setProjects] = useState([]);
 
